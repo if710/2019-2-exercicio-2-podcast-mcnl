522d7426dc2cbf3b5d091c17d669c8e7fed7f977
mcnl <mcnl.cin.ufpe.br>
2019-09-17 20:28:29 -0300

    First Commit with link of podcast

--- PodcastPlayer/app/src/main/java/br/ufpe/cin/android/podcast/MainActivity.kt
+++ PodcastPlayer/app/src/main/java/br/ufpe/cin/android/podcast/MainActivity.kt
@@ -8,5 +8,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+        //https://s3-us-west-1.amazonaws.com/podcasts.thepolyglotdeveloper.com/podcast.xml
     }
 }
